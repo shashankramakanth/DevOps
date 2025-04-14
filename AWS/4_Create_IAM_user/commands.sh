faf02a4f3659f32cf50bbc375dbfac811f73fafd
Shashank Ramakanth <11318192+shashankramakanth@users.noreply.github.com>
2025-04-14 06:43:57 +0530

    IAM

--- AWS/4_Create_IAM_user/commands.sh
+++ AWS/4_Create_IAM_user/commands.sh
@@ -13,4 +13,9 @@ aws iam create-instance-profile --instance-profile-name EC2-S3-FullAccess-Role
 #5. Attach the role to an EC2 instance
 aws ec2 associate-iam-instance-profile --instance-id <> --iam-instance-profile Name=EC2-S3-FullAccess-Role
 
+#6. Assign temporary password with password reset required
+aws iam create-login-profile --user-name awsadmin --password "TemporaryPassword123!" --password-reset-required
 
+#7. Create an IAM user and add to admin group
+aws iam create-user --user-name awsadmin && aws iam add-user-to-group --user-name awsadmin -
+-group-name admin && aws iam create-login-profile --user-name awsadmin --password "TemporaryPassword123" --password-reset-required
