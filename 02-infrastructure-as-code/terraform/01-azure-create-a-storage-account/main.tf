2ce1158b9450b9d3f5c23239feca8b3a16d85920
Shashank Ramakanth <shashank.1293@gmail.com>
2025-08-12 20:55:24 +0530

    modify main.tf to refer variables

--- 02-infrastructure-as-code/terraform/01-azure-create-a-storage-account/main.tf
+++ 02-infrastructure-as-code/terraform/01-azure-create-a-storage-account/main.tf
@@ -11,21 +11,21 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "subscription-id" #replace with your subscription id
+  subscription_id = "${var.subscription_id}" 
   resource_provider_registrations = "none" #for sandbox account
 }
 
 data "azurerm_resource_group" "az-rg-sandbox-01" {
-  name     = "resource-group-name" #replace with your resource group name
+  name     = "${var.resource_group_name}" 
  # location = "southcentralus"
 }
 
 resource "azurerm_storage_account" "az-sa-sandbox-01" {
-  name                     = "storage-account-name " #must be globally unique
+  name                     = "${var.storage_account_name}" #must be globally unique
   resource_group_name      = data.azurerm_resource_group.az-rg-sandbox-01.name #implicit dependency
   location                 = data.azurerm_resource_group.az-rg-sandbox-01.location #implicit dependency
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "sandbox"
