1cbeba47397890404b66049cce26d9fa130ce791
Shashank Ramakanth <shashank.1293@gmail.com>
2025-08-19 05:27:39 +0530

    terraform variables

--- 02-infrastructure-as-code/terraform/03-azure-terraform-variables/variables.tf
+++ 02-infrastructure-as-code/terraform/03-azure-terraform-variables/variables.tf
@@ -1,17 +1,17 @@
 variable "subscription_id" {
   description = "The ID of the Azure subscription where the storage account will be created."
   type        = string
-  default     = "0cfe2870-d256-4119-b0a3-16293ac11bdc" # replace with your subscription id
+  default     = "<>" # replace with your subscription id
 }
 
 variable "resource_group_name" {
   description = "value of the resource group name where the storage account will be created."
   type        = string
-  default     = "1-4c71411a-playground-sandbox" # replace with your resource group name
+  default     = "<>" # replace with your resource group name
 }
 
 variable "storage_account_name" {
     description = "The name of the storage account. Must be globally unique."
     type        = string
-    default     = "azsatf4c71411a" # replace with your storage account name
+    default     = "<>" # replace with your storage account name
 }
