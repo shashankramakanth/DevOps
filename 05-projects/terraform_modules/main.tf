587d2b04710fc0f8fc8b3d1d17962017cd8a1bb5
Shashank Ramakanth <shashank.1293@gmail.com>
2025-09-07 15:09:40 +0530

    update folder structure

--- 05-projects/terraform_modules/main.tf
+++
@@ -1,3 +0,0 @@
-data "azurerm_resource_group" "example" {
-  name = var.resource_group_name
-}
--- 05-projects/terraform_modules/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
-  type        = string
-  default     = "missingresourcegroupname"
-}
